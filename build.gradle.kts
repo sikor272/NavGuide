b813200615b42d2a0ec4ed1c198089358efac4ee
caleed <ja.arkuszewski@gmail.com>
2019-12-04 17:57:38 +0100

    fix jjwt version

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.jsonwebtoken:jjwt")
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("com.auth0:java-jwt:3.3.0")
 
     runtimeOnly("mysql:mysql-connector-java")
