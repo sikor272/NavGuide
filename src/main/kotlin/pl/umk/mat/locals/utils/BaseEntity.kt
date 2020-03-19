522f279898cb36c39e9b81d1153456382931c258
Taraj <kbachon97@gmail.com>
2020-03-19 19:39:08 +0100

    add base entity

---
+++ src/main/kotlin/pl/umk/mat/locals/utils/BaseEntity.kt
@@ -0,0 +1,29 @@
+package pl.umk.mat.locals.utils
+
+import org.springframework.data.domain.Persistable
+import javax.persistence.*
+
+@MappedSuperclass
+abstract class BaseEntity(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private val id: Long = -1
+) : Persistable<Long> {
+
+    override fun getId() = id
+
+    override fun isNew() = id == -1L
+
+    final override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (id != (other as BaseEntity).id) return false
+        return true
+    }
+
+    final override fun hashCode(): Int {
+        return 31
+    }
+
+    override fun toString() = "Entity of type ${this.javaClass.name} with id: $id"
+}
