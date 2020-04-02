ad3ef85be48624ea6ecb3701ebfbb7a9b6f0eb8c
sikor272 <dawidsikorski272@gmail.com>
2020-04-02 13:49:39 +0200

    bugfix

--- src/main/kotlin/pl/umk/mat/locals/message/MessageRepository.kt
+++ src/main/kotlin/pl/umk/mat/locals/message/MessageRepository.kt
@@ -2,7 +2,11 @@ package pl.umk.mat.locals.message
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import pl.umk.mat.locals.offer.purchase.NewPurchaseRequest
+import pl.umk.mat.locals.offer.purchase.PurchaseRequest
 
 
 @Repository
-interface MessageRepository : CrudRepository<Message, Long>
+interface MessageRepository : CrudRepository<Message, Long> {
+    fun getAllByPurchaseRequest(purchaseRequest: PurchaseRequest): List<Message>
+}
--- src/main/kotlin/pl/umk/mat/locals/message/MessageService.kt
+++ src/main/kotlin/pl/umk/mat/locals/message/MessageService.kt
@@ -16,26 +16,21 @@ class MessageService(
     fun addNewMessage(user: User, newMessage: NewMessage, purchaseId: Long) {
         val purchaseRequest = purchaseRequestRepository.findByIdOrThrow(purchaseId)
         if (purchaseRequest.traveler.id == user.id || purchaseRequest.offer.owner.user.id == user.id)
-            purchaseRequestRepository.save(
-                    purchaseRequest.copy(
-                            chatMessage = purchaseRequest.chatMessage.plus(messageRepository.save(
-                                    Message(
-                                            author = user,
-                                            description = newMessage.description,
-                                            date = Date(),
-                                            purchaseRequest = purchaseRequest
-                                    )
-                            ))
+            messageRepository.save(
+                    Message(
+                            author = user,
+                            description = newMessage.description,
+                            date = Date(),
+                            purchaseRequest = purchaseRequest
                     )
             )
-
         throw AuthException("You don't have permission to add messages!")
     }
 
     fun getAllMessagesForPurchaseRequest(user: User, id: Long): List<MessageDto> {
         val purchaseRequest = purchaseRequestRepository.findByIdOrThrow(id)
         if (purchaseRequest.traveler.id == user.id || purchaseRequest.offer.owner.user.id == user.id)
-            return purchaseRequest.chatMessage.map { MessageDto(it) }
+            return messageRepository.getAllByPurchaseRequest(purchaseRequest).map { MessageDto(it) }
         throw AuthException("You don't have permission to display messages!")
     }
 }
